3c8b9f9e36b39ddb19a3b1da7598ff7cd8d74508
Glitch (glitch-hello-website) <none>
2022-03-30 18:05:51 +0000

    🍇🌜 Checkpoint ./.glitch-assets:43155125/8463 ./script.js:43155125/182

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let ctx= c.getContext("2d");
 
 let loadImage=(src,callback)=> {
  let img = document.createElement("img");
- img.onload={} => callback(img);
- img.src=src;
+ img.onload=() => callback(img);
+ img.src="https://cdn.glitch.global/61210fac-b0c7-44d0-a5f7-947d0c908b01/thumbnails%2Fidle.png?1648663167296";
 }
 
 let imagePath= (frameNumber,animation)=> {
@@ -12,8 +12,8 @@ let imagePath= (frameNumber,animation)=> {
 }
 let frames={
  idle:[1,2,3,4,5,6,7,8],
- kick:[1,2,3,4,5,6,7],
- pucnh:[1,2,3,4,5,6,7],
+ kick:[kick1,kick2,kick3,kick4,kick5,kick6,kick7],
+ punch:[punch1,punch2,punch3,punch4,punch5,punch6,punch7],
 }
 let loadImages=(callback)=>{
  let images={idle:[], kick:[], punch:[]};
