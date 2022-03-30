bc265b96b9b4851096ad068145164c4ce79fdcad
Glitch (glitch-hello-website) <none>
2022-03-30 17:53:52 +0000

    🛬🎺 Checkpoint ./index.html:43155125/19 ./script.js:43155125/7

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let loadImage=(src,callback)=> {
 }
 
 let imagePath= (frameNumber,animation)=> {
- return "/assets"+animation +"/"+frameNumber+".png";
+ return ""+animation +"/"+frameNumber+".png";
 }
 let frames={
  idle:[1,2,3,4,5,6,7,8],
